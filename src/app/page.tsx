345f1f8431eb2a11692d4a5a9a0fcc654999a722
Punnarun <Punzaza.yonvilas@gmail.com>
2024-09-19 02:42:01 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Page</h1>
+      {/* GIF */}
+      <img
+        src="/truck.gif"
+        alt="Truck GIF"
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
+}
